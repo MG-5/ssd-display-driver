7262c73df5d8b8038bcced9212775b4215d15da5
Maximilian Grau <maximilian.grau@st.ovgu.de>
2022-03-10 00:25:25 +0100

    Add reverse byte variant for all architectures

--- include/oled-driver/DualRenderer.hpp
+++ include/oled-driver/DualRenderer.hpp
@@ -57,11 +57,18 @@ private:
                 size_t currentPosition = page * ScreenWidth + widthIndex;
                 byteData = vramToRead[currentPosition];
 
-                // reverse bit order and shift them by 24 due uint32_t return type
-                byteData = __RBIT(byteData) >> 24;
+                uint8_t reversedByteData = 0;
+                for (size_t i = 0; i <= 3; i++)
+                    reversedByteData |= (byteData & (1 << i)) << (7 - 2 * i);
+
+                for (size_t i = 4; i <= 7; i++)
+                    reversedByteData |= (byteData & (1 << i)) >> ((i - 3) * 2 - 1);
+
+                // reverse bit order and shift them by 24 due uint32_t
+                // return type byteData = __RBIT(byteData) >> 24; // only for ARM arch
 
                 size_t dualRamIndex = page + widthIndex * ScreenHeight / 8;
-                intermediateVram[dualRamIndex] = byteData;
+                intermediateVram[dualRamIndex] = reversedByteData;
             }
         }
     }
